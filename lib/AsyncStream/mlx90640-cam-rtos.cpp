bd3f2875d5f67ae9f1c3601ee6c68a0ea61dda9f
muzkatnuzz <muzkatnuzz@outlook.com>
2021-04-08 22:10:53 +0200

    Remove non working color mapping code

--- lib/AsyncStream/mlx90640-cam-rtos.cpp
+++ lib/AsyncStream/mlx90640-cam-rtos.cpp
@@ -77,7 +77,6 @@ const int FPS = 4;
 // We will handle web client requests every 100 ms (10 Hz)
 const int WSINTERVAL = 100;
 
-
 // ======== Camera settings ========================================
 // camera raw rows/cols
 const uint8_t COLS = 32;
@@ -88,44 +87,6 @@ const uint8_t INTERPOLATION_FACTOR_ROWS = 10;
 const uint8_t INTERPOLATION_FACTOR_COLS = 10;
 const uint16_t INTERPOLATED_ROWS = INTERPOLATION_FACTOR_ROWS * ROWS;
 const uint16_t INTERPOLATED_COLS = INTERPOLATION_FACTOR_COLS * COLS;
-
-// ======== Colorize temperature readings ==========================
-//low range of the sensor (this will be blue on the screen)
-const float MINTEMP = 20;
-
-//high range of the sensor (this will be red on the screen)
-const float MAXTEMP = 35;
-
-//the colors we will be using
-const uint16_t camColors[] = {0x480F,
-0x400F,0x400F,0x400F,0x4010,0x3810,0x3810,0x3810,0x3810,0x3010,0x3010,
-0x3010,0x2810,0x2810,0x2810,0x2810,0x2010,0x2010,0x2010,0x1810,0x1810,
-0x1811,0x1811,0x1011,0x1011,0x1011,0x0811,0x0811,0x0811,0x0011,0x0011,
-0x0011,0x0011,0x0011,0x0031,0x0031,0x0051,0x0072,0x0072,0x0092,0x00B2,
-0x00B2,0x00D2,0x00F2,0x00F2,0x0112,0x0132,0x0152,0x0152,0x0172,0x0192,
-0x0192,0x01B2,0x01D2,0x01F3,0x01F3,0x0213,0x0233,0x0253,0x0253,0x0273,
-0x0293,0x02B3,0x02D3,0x02D3,0x02F3,0x0313,0x0333,0x0333,0x0353,0x0373,
-0x0394,0x03B4,0x03D4,0x03D4,0x03F4,0x0414,0x0434,0x0454,0x0474,0x0474,
-0x0494,0x04B4,0x04D4,0x04F4,0x0514,0x0534,0x0534,0x0554,0x0554,0x0574,
-0x0574,0x0573,0x0573,0x0573,0x0572,0x0572,0x0572,0x0571,0x0591,0x0591,
-0x0590,0x0590,0x058F,0x058F,0x058F,0x058E,0x05AE,0x05AE,0x05AD,0x05AD,
-0x05AD,0x05AC,0x05AC,0x05AB,0x05CB,0x05CB,0x05CA,0x05CA,0x05CA,0x05C9,
-0x05C9,0x05C8,0x05E8,0x05E8,0x05E7,0x05E7,0x05E6,0x05E6,0x05E6,0x05E5,
-0x05E5,0x0604,0x0604,0x0604,0x0603,0x0603,0x0602,0x0602,0x0601,0x0621,
-0x0621,0x0620,0x0620,0x0620,0x0620,0x0E20,0x0E20,0x0E40,0x1640,0x1640,
-0x1E40,0x1E40,0x2640,0x2640,0x2E40,0x2E60,0x3660,0x3660,0x3E60,0x3E60,
-0x3E60,0x4660,0x4660,0x4E60,0x4E80,0x5680,0x5680,0x5E80,0x5E80,0x6680,
-0x6680,0x6E80,0x6EA0,0x76A0,0x76A0,0x7EA0,0x7EA0,0x86A0,0x86A0,0x8EA0,
-0x8EC0,0x96C0,0x96C0,0x9EC0,0x9EC0,0xA6C0,0xAEC0,0xAEC0,0xB6E0,0xB6E0,
-0xBEE0,0xBEE0,0xC6E0,0xC6E0,0xCEE0,0xCEE0,0xD6E0,0xD700,0xDF00,0xDEE0,
-0xDEC0,0xDEA0,0xDE80,0xDE80,0xE660,0xE640,0xE620,0xE600,0xE5E0,0xE5C0,
-0xE5A0,0xE580,0xE560,0xE540,0xE520,0xE500,0xE4E0,0xE4C0,0xE4A0,0xE480,
-0xE460,0xEC40,0xEC20,0xEC00,0xEBE0,0xEBC0,0xEBA0,0xEB80,0xEB60,0xEB40,
-0xEB20,0xEB00,0xEAE0,0xEAC0,0xEAA0,0xEA80,0xEA60,0xEA40,0xF220,0xF200,
-0xF1E0,0xF1C0,0xF1A0,0xF180,0xF160,0xF140,0xF100,0xF0E0,0xF0C0,0xF0A0,
-0xF080,0xF060,0xF040,0xF020,0xF800,};
-
-uint16_t displayPixelWidth, displayPixelHeight;
 
 // ======== Server Connection Handler Task ==========================
 void mjpegCB(void *pvParameters)
@@ -154,9 +115,6 @@ void mjpegCB(void *pvParameters)
   //  Registering webserver handling routines
   server->on("/stream", HTTP_GET, handleJPGSstream);
   server->on("/jpg", HTTP_GET, handleJPG);
-
-  // TODO should be done by host Starting webserver
-  // server.begin();
 
   noActiveClients = 0;
 
@@ -214,6 +172,7 @@ boolean isConnected()
   return (true);
 }
 
+// color calculation
 // start with some initial colors
 float minTemp = 20.0;
 float maxTemp = 40.0;
@@ -280,28 +239,10 @@ uint16_t calculateColor(float val)
   return color565(red, green, blue);
 }
 
-// Use mapping table to map given temperature to color 
-// https://github.com/adafruit/Adafruit_MLX90640/blob/master/examples/MLX90640_arcadaCam/MLX90640_arcadaCam.ino
-uint16_t mapColor(float val)
-{
-  float t = val;
-
-  t = min(t, MAXTEMP);
-  t = max(t, MINTEMP);
-
-  // convert measured temperatures to color
-  uint8_t colorIndex = map(t, MINTEMP, MAXTEMP, 0, 255);
-  colorIndex = constrain(colorIndex, 0, 255);
-
-  // replace measured temperature by color
-  uint16_t color = camColors[colorIndex];
-  return color;
-}
-
 // Current frame information
 volatile uint32_t frameNumber;
-volatile size_t camSize;  // size of the current frame, byte
-volatile char *camBuf; // pointer to the current frame
+volatile size_t camSize; // size of the current frame, byte
+volatile char *camBuf;   // pointer to the current frame
 
 // ==== RTOS task to grab frames from the camera =========================
 void camCB(void *pvParameters)
@@ -357,9 +298,9 @@ void camCB(void *pvParameters)
     float *mlx90640To = NULL;
     mlx90640To = allocateMemory(mlx90640To, ROWS * COLS * sizeof(float));
     getFrame(mlx90640To);
-    
+
     //log_d("camCB interpolation begin: Allocate Memory. Largest heap size:\t %zu", heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT)); // as from https://github.com/espressif/esp32-camera/blob/master/conversions/to_jpg.cpp
-    float *interpolated_mlx90640To = NULL; 
+    float *interpolated_mlx90640To = NULL;
     interpolated_mlx90640To = allocateMemory(interpolated_mlx90640To, INTERPOLATED_ROWS * INTERPOLATED_COLS * sizeof(float));
 
     interpolate_image_nearest_neighbour(mlx90640To, ROWS, COLS, interpolated_mlx90640To, INTERPOLATED_ROWS, INTERPOLATED_COLS);
@@ -368,33 +309,23 @@ void camCB(void *pvParameters)
     uint16_t *mlx90640ToColors = NULL;
     mlx90640ToColors = allocateMemory(mlx90640ToColors, INTERPOLATED_ROWS * INTERPOLATED_COLS * sizeof(uint16_t));
     //log_d("camCB interpolation end: Allocate Memory. Largest heap size:\t %zu", heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT)); // as from https://github.com/espressif/esp32-camera/blob/master/conversions/to_jpg.cpp
-    
+
     // convert to actual colors
     for (uint16_t h = 0; h < INTERPOLATED_ROWS; h++)
     {
       for (uint16_t w = 0; w < INTERPOLATED_COLS; w++)
       {
-        // choose which color conversion fits better - mapping table or calculation
-        if (false)
-        {
-          mlx90640ToColors[h * INTERPOLATED_COLS + w] = mapColor(interpolated_mlx90640To[h * INTERPOLATED_COLS + w]);
-        }
-        else
-        {
-          mlx90640ToColors[h * INTERPOLATED_COLS + w] = calculateColor(interpolated_mlx90640To[h * INTERPOLATED_COLS + w]);
-        }
+        mlx90640ToColors[h * INTERPOLATED_COLS + w] = calculateColor(interpolated_mlx90640To[h * INTERPOLATED_COLS + w]);
       }
     }
-    
-    free(interpolated_mlx90640To);
-    //log_d("Allocate Memory. Largest heap size: %zu", heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT)); // as from https://github.com/espressif/esp32-camera/blob/master/conversions/to_jpg.cpp
-    fbs = allocateMemory(fbs, INTERPOLATED_ROWS * INTERPOLATED_COLS * sizeof(uint16_t));
-    //log_d("Memcopy. Largest heap size: %zu", heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT)); // as from https://github.com/espressif/esp32-camera/blob/master/conversions/to_jpg.cpp
 
+    free(interpolated_mlx90640To);
+    
     //  Copy current frame into local buffer
+    fbs = allocateMemory(fbs, INTERPOLATED_ROWS * INTERPOLATED_COLS * sizeof(uint16_t));
     memcpy(fbs, mlx90640ToColors, INTERPOLATED_ROWS * INTERPOLATED_COLS * sizeof(uint16_t));
     free(mlx90640ToColors);
-    
+
     //  Let other tasks run and wait until the end of the current frame rate interval (if any time left)
     taskYIELD();
     vTaskDelayUntil(&xLastWakeTime, xFrequency);
@@ -464,7 +395,6 @@ float *allocateMemory(float *aPtr, size_t aSize)
   }
   return ptr;
 }
-
 
 // ==== Memory allocator that takes advantage of PSRAM if present =======================
 uint16_t *allocateMemory(uint16_t *aPtr, size_t aSize)
