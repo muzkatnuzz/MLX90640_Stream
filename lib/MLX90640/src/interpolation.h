dce86ad5ecc0cb14a79caedb1a6c186b2b6d9279
muzkatnuzz <muzkatnuzz@outlook.com>
2021-04-08 21:51:10 +0200

    Cleanup unused interpolation algo

--- lib/MLX90640/src/interpolation.h
+++ lib/MLX90640/src/interpolation.h
@@ -1,11 +1,3 @@
 #include <Arduino.h>
 
-float get_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y);
-void set_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y, float f);
-void get_adjacents_1d(float *src, float *dest, uint8_t rows, uint8_t cols, int8_t x, int8_t y);
-void get_adjacents_2d(float *src, float *dest, uint8_t rows, uint8_t cols, int8_t x, int8_t y);
-float cubicInterpolate(float p[], float x);
-float bicubicInterpolate(float p[], float x, float y);
-void interpolate_image(float *src, uint8_t src_rows, uint8_t src_cols,
-                       float *dest, uint8_t dest_rows, uint8_t dest_cols);
 void interpolate_image_nearest_neighbour(float *src, uint16_t src_width, uint16_t src_heigth, float *dest, uint16_t dest_width, uint16_t dest_height);
